08c472afdcd830b8f32301b8d7cba797de6ceebe
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-09-24 13:54:57 -0300

    Creado script de variables y constantes

---
+++ 01-reforzamiento/src/bases/01-const-let.ts
@@ -0,0 +1,8 @@
+const firstName: string = 'Cristian';
+const lastName: string = 'Cajiao';
+
+let diceNumber = 5;
+diceNumber = 3;
+const containsLetterCMin = lastName.includes('c');
+
+console.log({containsLetterCMin, diceNumber, firstName, lastName});
