f21b2e8d570e9555aeb55cccd4d25c67ae4ceb74
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-09-25 14:42:00 -0300

    Modificado script de funciones para incluir funciones con múltiples retornos

--- 01-reforzamiento/src/bases/05-functions.ts
+++ 01-reforzamiento/src/bases/05-functions.ts
@@ -2,9 +2,7 @@ function greet(name: string): string {
   return `Hola ${name}`;
 }
 
-const greet2 = (name: string): string => {
-  return `Hola ${name}`;
-}; 
+const greet2 = (name: string) => `Hola ${name}`;
 
 const message = greet('John');
 const message2 = greet2('Perico');
@@ -12,21 +10,29 @@ const message2 = greet2('Perico');
 console.log(message);
 console.log(message2);
 
-function getUser() {
+interface User {
+  uid: string;
+  username: string;
+}
+
+function getUser(): User {
   return {
     uid: 'ABC-123',
     username: 'miusuario'
   }
 }
 
-const getUser2 = () => {
-  return {
-    uid: 'ABC-123',
-    username: 'miusuario'
-  }
-};
+const getUser2 = () => ({
+  uid: 'ABC-123',
+  username: 'miusuario'
+});
 
 const user = getUser();
 const user2 = getUser2();
 
 console.log(user, user2);
+
+/* Callbacks */
+
+const myNumbers: number[ ]= [1,2,3,4,5,6];
+myNumbers.forEach((value, index, array) => console.log);
