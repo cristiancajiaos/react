a374712ec3a41b6c9a4706f140873cecc3d239d1
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-09-30 12:24:05 -0300

    Agregado script de async/await

--- 01-reforzamiento/src/main.ts
+++ 01-reforzamiento/src/main.ts
@@ -8,7 +8,8 @@ import './style.css'
 // import './bases/07-array-destructuring';
 // import './bases/08-imp-exp';
 // import './bases/09-promises.ts';
-import './bases/10-fetch-api';
+// import './bases/10-fetch-api';
+import './bases/11-async-await';
 
 // Tarea de Importación y Exportación
 // import { Owner } from './data/heroes.data';
