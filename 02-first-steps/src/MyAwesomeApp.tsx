2142c6f94f0c744c60fd7a334b330312f4dc142a
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-10-01 12:49:33 -0300

    Definición de variables en el componente MyAwesomeApp

--- 02-first-steps/src/MyAwesomeApp.tsx
+++ 02-first-steps/src/MyAwesomeApp.tsx
@@ -1,8 +1,26 @@
+const firstName = 'Cristian';
+const lastName = 'Cajiao';
+
+const favoriteGames = ['Angry Birds', 'Grindstone', 'Wildfrost'];
+const isActive = true;
+
+const address = {
+  zipCode: 'AB-123',
+  country: 'Chile'
+};
+
 export const MyAwesomeApp = () => {
   return (
     <>
-      <h1>Cristian</h1>
-      <h3>Cajiao</h3>
+      <h1>{firstName}</h1>
+      <h3>{lastName}</h3>
+
+      <p>{favoriteGames.join(',')}</p>
+      <p>{2 + 2}</p>
+
+      <h1>{isActive ? 'Activo' : 'No activo'}</h1>
+
+      <p>{JSON.stringify(address)}</p>
     </>
   );
 }
